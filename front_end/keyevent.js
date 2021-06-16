6b51255a3422a5ccfb6d5dadd58b4c552bb80717
Ankit Juneja <ankitjuneja4002@gmail.com>
2021-06-16 12:20:30 +0530

    Add files via upload

---
+++ front_end/keyevent.js
@@ -0,0 +1,274 @@
+var A = document.getElementById('one');
+var S = document.getElementById('two');
+var D = document.getElementById('three');
+var F = document.getElementById('four');
+var G = document.getElementById('five');
+var H = document.getElementById('six');
+var J = document.getElementById('sev');
+var K = document.getElementById('eght');
+var L = document.getElementById('nine');
+
+//jbibibiubbibinihihiinijniubiubibhuvuvu
+
+var clap = document.getElementById('clap');
+var boom = document.getElementById('boom');
+var tink = document.getElementById('tink');
+var snare = document.getElementById('snare');
+var ride = document.getElementById('ride');
+var hihat = document.getElementById('hihat');
+var kick = document.getElementById('kick');
+var tom = document.getElementById('tom');
+var openhat = document.getElementById('open');
+
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 65){
+        
+        A.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 65){
+        A.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 83){
+        
+        S.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 83){
+        S.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 68){
+        
+        D.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 68){
+        D.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 70){
+        
+        F.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 70){
+        F.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 71){
+        
+        G.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 71){
+        G.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 72){
+        
+        H.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 72){
+        H.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 74){
+        
+        J.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 74){
+        J.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 75){
+        
+        K.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 75){
+        K.style.outline = "none";
+        
+    }   
+
+});
+
+document.body.addEventListener('keydown', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 76){
+        
+        L.style.outline = "2px solid yellow";
+    }   
+
+});
+
+document.body.addEventListener('keyup', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 76){
+        L.style.outline = "none";
+        
+    }   
+
+});
+
+
+//nidbfgiberbgeuirngerngineihgoeheorngone
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 65){
+        
+        clap.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 83){
+        
+        hihat.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 68){
+        
+        kick.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 70){
+        
+        openhat.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 71){
+        
+        boom.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 72){
+        
+        ride.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 74){
+        
+        snare.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 75){
+        
+        tom.play();
+    }   
+
+});
+
+document.body.addEventListener('keypress', function(e){
+    var keycode = e.keyCode;
+    if(keycode === 76){
+        
+        tink.play();
+    }   
+
+});
+
+
+
+
+
+
